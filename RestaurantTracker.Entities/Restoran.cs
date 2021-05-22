4e147f814f12312f0610f19d398167cf4382dc76
akif <akif414181@gmail.com>
2021-05-22 21:50:10 +0300

    Add Restoran.cs

---
+++ RestaurantTracker.Entities/Restoran.cs
@@ -0,0 +1,10 @@
+namespace RestaurantTracker.Entities
+{
+    public class Restoran
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Type_of_Food { get; set; }
+    }
+}
